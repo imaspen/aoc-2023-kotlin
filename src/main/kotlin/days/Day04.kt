a7e66b855114c42a454d14aeeae7a79db8e39acd
Aspen <hi@imaspen.dev>
2023-12-04 11:15:40 +0000

    Day 04

--- src/main/kotlin/days/Day04.kt
+++ src/main/kotlin/days/Day04.kt
@@ -1,11 +1,50 @@
 package days
 
+import kotlin.math.pow
+
 class Day04 : Day {
+    private class Ticket(private val winningNumbers: Set<Int>, private val gameNumbers: Set<Int>) {
+        fun winningCount(): Int {
+            return gameNumbers.intersect(winningNumbers).count()
+        }
+
+        fun getScore(): Double {
+            val count = winningCount()
+            if (count == 0) return 0.0
+
+            return 2.0.pow(count - 1.0)
+        }
+
+        override fun toString(): String {
+            return "Ticket(winningNumbers: $winningNumbers, gameNumbers: $gameNumbers)"
+        }
+    }
+
+    private val numberMatcher = Regex("""\d+""")
+    private val tickets = javaClass.getResource("/day04.txt")!!.readText().lines().map {line ->
+        val sets = line.split(": ")[1].split(" | ").map { part ->
+            val matches = numberMatcher.findAll(part)
+            val set = matches.map { matchResult -> matchResult.value.toInt() }.toSet()
+            // I've made the assumption that duplicates don't exist, but we should check that assumption holds
+            assert(matches.count() == set.count()) { "Duplicate numbers exist, rethink this solution" }
+            set
+        }
+        Ticket(sets[0], sets[1])
+    }
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        val score = tickets.sumOf { ticket -> ticket.getScore() }
+        return "%.0f".format(score)
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val ticketCounts = tickets.map { 1 }.toMutableList()
+        tickets.forEachIndexed { index, ticket ->
+            val count = ticket.winningCount()
+            for (i in 1..count) {
+                ticketCounts[index + i] += ticketCounts[index]
+            }
+        }
+        return ticketCounts.sum().toString()
     }
 }
--- src/test/kotlin/days/Day04Test.kt
+++ src/test/kotlin/days/Day04Test.kt
@@ -7,9 +7,11 @@ class Day04Test {
 
     @Test
     fun part1() {
+        assertEquals("13", Day04().part1())
     }
 
     @Test
     fun part2() {
+        assertEquals("30", Day04().part2())
     }
 }
