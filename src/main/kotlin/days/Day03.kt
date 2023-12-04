9173a4bc78a87e49ee0707b08db4fc37ba732d9c
Aspen <hi@imaspen.dev>
2023-12-04 01:24:37 +0000

    Day 03

--- src/main/kotlin/days/Day03.kt
+++ src/main/kotlin/days/Day03.kt
@@ -1,11 +1,73 @@
 package days
 
+typealias Point = Pair<Int, Int>
+
+
 class Day03 : Day {
+    private class PartNumber(private val startPosition: Point, val number: Int) {
+        override fun toString(): String {
+            return "PartNumber(startPosition: $startPosition, number: $number)"
+        }
+    }
+
+    private val lines = javaClass.getResource("/day03.txt")!!.readText().lines()
+    private val numberMatcher = Regex("""\d+""")
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        val positions = mutableMapOf<Point, PartNumber>()
+        val symbols = mutableListOf<Point>()
+        val symbolMatcher = Regex("""[^\d.]""")
+        lines.withIndex().forEach { lineWithIndex ->
+            val y = lineWithIndex.index
+            val line = lineWithIndex.value
+            numberMatcher.findAll(line).forEach { match ->
+                val partNumber = PartNumber(Point(match.range.first, y), match.value.toInt())
+                match.range.forEach { x -> positions[Point(x, y)] = partNumber }
+            }
+            symbolMatcher.findAll(line).forEach { match -> symbols.add(Point(match.range.first, y)) }
+        }
+        val validParts = hashSetOf<PartNumber>()
+        symbols.forEach { position ->
+            for (y in -1..1) {
+                for (x in -1..1) {
+                    val part = positions[Point(position.first + x, position.second + y)]
+                    if (part != null) {
+                        validParts.add(part)
+                    }
+                }
+            }
+        }
+        return validParts.sumOf { partNumber -> partNumber.number }.toString()
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val positions = mutableMapOf<Point, PartNumber>()
+        val possibleGears = mutableListOf<Point>()
+        val gearMatcher = Regex("""\*""")
+        lines.withIndex().forEach { lineWithIndex ->
+            val y = lineWithIndex.index
+            val line = lineWithIndex.value
+            numberMatcher.findAll(line).forEach { match ->
+                val partNumber = PartNumber(Point(match.range.first, y), match.value.toInt())
+                match.range.forEach { x -> positions[Point(x, y)] = partNumber }
+            }
+            gearMatcher.findAll(line).forEach { match -> possibleGears.add(Point(match.range.first, y)) }
+        }
+        return possibleGears.sumOf { position ->
+            val validParts = hashSetOf<PartNumber>()
+            for (y in -1..1) {
+                for (x in -1..1) {
+                    val part = positions[Point(position.first + x, position.second + y)]
+                    if (part != null) {
+                        validParts.add(part)
+                    }
+                }
+            }
+            if (validParts.count() == 2) {
+                validParts.fold(1L) { acc, part -> acc * part.number }
+            } else {
+                0
+            }
+        }.toString()
     }
 }
--- src/test/kotlin/days/Day03Test.kt
+++ src/test/kotlin/days/Day03Test.kt
@@ -7,9 +7,11 @@ class Day03Test {
 
     @Test
     fun part1() {
+        assertEquals("4361", Day03().part1())
     }
 
     @Test
     fun part2() {
+        assertEquals("467835", Day03().part2())
     }
 }
