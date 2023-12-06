280939863b144a9423b4effe0bb34e45a7493fb3
Aspen <hi@imaspen.dev>
2023-12-06 10:33:52 +0000

    Day 06

--- src/main/kotlin/days/Day06.kt
+++ src/main/kotlin/days/Day06.kt
@@ -1,11 +1,27 @@
 package days
 
+import kotlin.math.*
+
 class Day06 : Day {
+    private val lines = javaClass.getResource("/day06.txt")!!.readText().lines()
+
+    private fun getWinCount(time: Double, distance: Double): Long {
+        val start = floor(1.0 + 0.5 * (time - sqrt(time.pow(2) - 4 * distance)))
+        val end = ceil(0.5 * (time + sqrt(time.pow(2) - 4 * distance)) - 1.0)
+        return (1 + end - start).roundToLong()
+    }
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        val numberMatcher = Regex("""\d+""")
+        val definitions = lines.map { numberMatcher.findAll(it).map { match -> match.value.toDouble() } }
+            .zipWithNext { a, b -> a.zip(b) }[0].toList()
+
+        // saw the brute force punishment coming for once and went algebraic from the start
+        return definitions.fold(1L) { acc, (time, distance) -> acc * getWinCount(time, distance) }.toString()
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val definitions = lines.map { it.replace(" ", "").split(":")[1].toDouble() }
+        return getWinCount(definitions[0], definitions[1]).toString()
     }
 }
--- src/test/kotlin/days/Day06Test.kt
+++ src/test/kotlin/days/Day06Test.kt
@@ -7,9 +7,11 @@ class Day06Test {
 
     @Test
     fun part1() {
+        assertEquals("288", Day06().part1())
     }
 
     @Test
     fun part2() {
+        assertEquals("71503", Day06().part2())
     }
 }
