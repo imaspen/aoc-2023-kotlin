40ea1f673ecf8da7d1217a419ed64510bb2f630f
Aspen <hi@imaspen.dev>
2023-12-08 12:29:59 +0000

    Day 08

--- src/main/kotlin/days/Day08.kt
+++ src/main/kotlin/days/Day08.kt
@@ -1,11 +1,85 @@
 package days
 
 class Day08 : Day {
+    data class Point(val left: Int, val right: Int) {
+        fun getNext(isLeft: Boolean): Int {
+            return if (isLeft) left else right
+        }
+    }
+
+    private fun codeToInt(string: String): Int {
+        return string.fold(0) { acc, char -> acc.shl(5) + char.code - 'A'.code }
+    }
+
+    private fun parseFile(isPart2: Boolean = false): Pair<List<Boolean>, Map<Int, Point>> {
+        val parts = javaClass.getResource(if (isPart2) "/day08.2.txt" else "/day08.1.txt")!!.readText().split("\n\n")
+        val instructions = parts[0].map { it == 'L' }
+        val definitions = parts[1].lines().associate { line ->
+            val (from, to) = line.split(" = ")
+            val sides = to.drop(1).dropLast(1).split(", ").map { codeToInt(it) }
+            (codeToInt(from) to Point(sides[0], sides[1]))
+        }
+        return Pair(instructions, definitions)
+    }
+
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        val (instructions, definitions) = parseFile()
+        val destination = codeToInt("ZZZ")
+        var position = 0
+        var steps = 0
+        var i = 0
+
+        while (position != destination) {
+            position = definitions[position]!!.getNext(instructions[i])
+            steps++
+            i++
+            if (i == instructions.count()) {
+                i = 0
+            }
+        }
+
+        return steps.toString()
+    }
+
+    private fun gcd(aIn: Long, bIn: Long): Long {
+        var t: Long
+        var a = aIn
+        var b = bIn
+        while (b != 0L) {
+            t = b
+            b = a % b
+            a = t
+        }
+        return a
+    }
+
+    private fun lcm(a: Long, b: Long): Long {
+        return a * b / gcd(a, b)
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val (instructions, definitions) = parseFile(true)
+
+        return definitions.keys.filter { (it and 0b11111) == 0 }.map { startPosition ->
+            var position = startPosition
+            var steps = 0
+            var i = 0
+
+            while (position and 0b11111 != 25) {
+                position = definitions[position]!!.getNext(instructions[i])
+                steps++
+                i++
+                if (i == instructions.count()) {
+                    i = 0
+                }
+            }
+
+            assert(definitions[position]!!.getNext(instructions[i]) == startPosition) {
+                "assumed that destination always takes us back to the beginning, is true on my input"
+            }
+
+            return@map steps.toLong()
+        }.reduce { a, b -> lcm(a, b) }.toString()
     }
 }
--- src/test/kotlin/days/Day08Test.kt
+++ src/test/kotlin/days/Day08Test.kt
@@ -7,9 +7,11 @@ class Day08Test {
 
     @Test
     fun part1() {
+        assertEquals("6", Day08().part1())
     }
 
     @Test
     fun part2() {
+        assertEquals("6", Day08().part2())
     }
 }
