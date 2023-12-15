b4095081e78f9644328d05469de5702f6fe2b502
Aspen <hi@imaspen.dev>
2023-12-15 10:22:13 +0000

    Day 15

--- src/main/kotlin/days/Day15.kt
+++ src/main/kotlin/days/Day15.kt
@@ -1,11 +1,33 @@
 package days
 
 class Day15 : Day {
+    private val parts = javaClass.getResource("/day15.txt")!!.readText().split(",")
+
+    private fun hashString(input: String): Int {
+        return input.fold(0) { acc, char ->
+            ((acc + char.code) * 17) % 256
+        }
+    }
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        return parts.sumOf { hashString(it) }.toString()
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val boxes = (0..<256).map { LinkedHashMap<String, Int>() }
+        for (instruction in parts) {
+            val (a, b) = instruction.split('=', '-')
+            val box = boxes[hashString(a)]
+            if (b.isEmpty()) {
+                box.remove(a)
+            } else {
+                box[a] = b.toInt()
+            }
+        }
+        return boxes.withIndex().sumOf {(boxIndex, box) ->
+            box.values.withIndex().sumOf { (slotIndex, power) ->
+                (1 + boxIndex) * (1 + slotIndex) * power
+            }
+        }.toString()
     }
 }
--- src/test/kotlin/days/Day15Test.kt
+++ src/test/kotlin/days/Day15Test.kt
@@ -7,9 +7,11 @@ class Day15Test {
 
     @Test
     fun part1() {
+        assertEquals("1320", Day15().part1())
     }
 
     @Test
     fun part2() {
+        assertEquals("145", Day15().part2())
     }
 }
